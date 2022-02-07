37d165e3438eb32ce0b32fa9ea11d6c01af289ab
GrolarKim <audwnswmf@naver.com>
2022-02-07 09:30:57 +0900

    admin에서 댓글 접근

--- place/admin.py
+++ place/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PlaceModel
+from .models import PlaceModel, PlaceComment
 
 # Register your models here.
 admin.site.register(PlaceModel)
+admin.site.register(PlaceComment)
